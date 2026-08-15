123283e9c69606b85118b3b8553b2f0ee6d5e6ed
mahak <vermamahak@gmail.com>
2026-08-15 17:34:37 +0530

    Update hero photo

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -125,7 +125,7 @@ export default function Hero() {
               {/* Portrait container */}
               <div className="relative w-80 h-96 md:w-96 md:h-[500px] flex items-center justify-center">
                 <img 
-                  src="/images/hero-sticker.png" 
+                  src="/images/hero-portrait.png" 
                   alt="Mahak Verma" 
                   style={{ width: '250px', height: 'auto' }}
                 />
